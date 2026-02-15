6c5bf4b4471ffcebf5532b1d087f8867fa727cf5
DotNicolasPenha <nicolaspenha45@gmail.com>
2026-02-15 17:45:33 -0300

    feat: manifest loader

--- internal/core/loader/manifest.go
+++ internal/core/loader/manifest.go
@@ -9,20 +9,31 @@ import (
 
 type Manifest struct {
 	Port            *int    `json:"port"`
-	NameApplication *string `json:"string"`
+	NameApplication *string `json:"nameApplication"`
 }
 
-func LoadManifest(path string) Manifest {
-	var manifestJson Manifest = reader.Json[Manifest](path)
+func LoadManifest(path string) *Manifest {
+	manifestJson := reader.Json[Manifest](path)
+
+	var errs []string
 
 	if manifestJson.Port == nil {
-		logger.Error(fmt.Sprintf("field 'port' is required in manifest of path: %s", path))
+		errs = append(errs, fmt.Sprintf("field 'port' is required in manifest of path: %s", path))
+	} else if *manifestJson.Port <= 0 || *manifestJson.Port > 65535 {
+		errs = append(errs, fmt.Sprintf("invalid port number in field 'port' of manifest: %s", path))
 	}
-	if *manifestJson.Port <= 0 || *manifestJson.Port > 65535 {
-		logger.Error(fmt.Sprintf("invalid port number to field 'port' of manifest: %s", path))
-	}
+
+
 	if manifestJson.NameApplication == nil || *manifestJson.NameApplication == "" {
-		logger.Error(fmt.Sprintf("field 'nameApplication' is required in manifest of path: %s", path))
+		errs = append(errs, fmt.Sprintf("field 'nameApplication' is required in manifest of path: %s", path))
 	}
+
+	if len(errs) > 0 {
+		for _, e := range errs {
+			logger.Error(e)
+		}
+		logger.Fatal("The manifest cannot be loaded because it has invalid fields.")
+	}
+
 	return manifestJson
 }
